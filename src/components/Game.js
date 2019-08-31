c9efed38712b63fd0f37beb0400f9ea1be5e720d
Casper <pnfcre@gmail.com>
2019-08-31 07:07:33 +0200

    :bug: Fixing a bug

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
 const Game = () => (
   <div className="dtc tc v-mid hidden" id="game">
-    <div className="f4 loading dots" id="stopwatch" />
+    <div className="f4" id="stopwatch" />
     <div className="hidden items-center ma4">
       <canvas className="br3 ba bw3 ma2 shadow-4" id="canvas" />
     </div>
